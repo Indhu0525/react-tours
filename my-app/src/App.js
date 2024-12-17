b2d954e9851f129e1beebdfb61264ab56b077eec
Indhu0525 <indu01401@hmail.com>
2024-12-17 19:22:57 +0530

    installation

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1 class="text-xl text-blue-600">hjdjhd</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
